adf7f9d38a1b6d93e5c0a27f245ae3835630bc84
Hells <96310786+He11Cut3@users.noreply.github.com>
2023-10-27 19:55:35 +0300

    v2.0

---
+++ HellsFilms/Infrastructure/Components/SerialFooterViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HellsFilms.Infrastructure.Components
+{
+    public class SerialFooterViewComponent : ViewComponent
+    {
+            private readonly DataContext _context;
+
+            public SerialFooterViewComponent(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<IViewComponentResult> InvokeAsync() => View(await _context.HellsFilmsSerials.ToListAsync());
+    }
+}
